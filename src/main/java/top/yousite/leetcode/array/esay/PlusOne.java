92b3d70cc9c72e22e47f657be8cd37a92d70612d
jianmen <97245251@qq.com>
2018-09-17 20:38:20 +0800

    init

---
+++ src/main/java/top/yousite/leetcode/array/esay/PlusOne.java
@@ -0,0 +1,26 @@
+package top.yousite.leetcode.array.esay;
+
+public class PlusOne {
+	
+	public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        //判断数组的各个位是否为9，是的话变为0；否则+1
+        for(int j =length-1 ;j>=0 ;j--){
+            if(digits[j] == 9){
+                digits[j] =0;
+                
+            }else{
+                digits[j]+=1;
+                return digits;
+            }
+        }
+        //判断第一位是否为0；(由于： 数组中每个元素只存储一个数字。）
+        //解决方法:用一个新的数组保存
+             //只有全为9999这样才会出现这种情况，数组初始化时全为0
+            int[] result=new int[digits.length+1];
+            result[0]=1;
+            return result;
+        
+    }
+
+}
