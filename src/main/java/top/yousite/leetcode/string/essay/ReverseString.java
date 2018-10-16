fcf59aca411e7fd018e54e0a60aa04154673f749
jianmen <97245251@qq.com>
2018-10-16 21:58:00 +0800

    344. 反转字符串

--- src/main/java/top/yousite/leetcode/string/essay/ReverseString.java
+++ src/main/java/top/yousite/leetcode/string/essay/ReverseString.java
@@ -4,7 +4,16 @@ public class ReverseString {
     /**
      * 344. 反转字符串
      */
-    public String reverseString(String s) {
+    /**
+     * 思路:
+     * solve-1.创建新的char数组，来存储原字符串
+     * solve-2.双指针，交换头尾
+     */
+
+    /**
+     * solve-1.创建新的char数组，来存储原字符串
+     */
+    /*public String reverseString(String s) {
         char[]ch = s.toCharArray();
         char[]newch = new char[ch.length];
         int count=0;
@@ -13,5 +22,26 @@ public class ReverseString {
 
         }
         return  new String(newch);
+    }*/
+
+    /**
+     * solve-2.双指针，交换头尾
+     * @param s
+     * @return
+     */
+    public String reverseString(String s) {
+        char[]ch =s.toCharArray();
+        int begin = 0;
+        int end = ch.length -1;
+        while (begin<end){
+            char temp = ch[begin];
+            ch[begin] = ch[end];
+            ch[end] = temp;
+            begin++;
+            end--;
+        }
+        return new String(ch);
     }
+
+
 }
