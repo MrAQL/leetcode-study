e6c82f680098aeef7d9a4f0c9b43b7daca5fb43a
jianmen <97245251@qq.com>
2018-10-18 20:58:49 +0800

    136. 只出现一次的数字

--- src/main/java/top/yousite/leetcode/hash/essay/SingleNumber.java
+++ src/main/java/top/yousite/leetcode/hash/essay/SingleNumber.java
@@ -7,19 +7,22 @@ import java.util.Map;
 public class SingleNumber {
     /**
      * 136. 只出现一次的数字
-     *
+     *  (除了某个元素只出现一次以外，其余每个元素均出现两次)
      */
 
     /**
-     * 哈希表
-     *      key:nums[i]
-     *      value:count
+     * solve-1.哈希表
+     *         key:nums[i]
+     *         value:count
      *
      * 然后遍历找出value = 1的
      *
+     * solve-2. 位运算
+     *        异或(^)：同为假，异为真
+     *
      */
     // time :O(n), space:O(n)
-    public int singleNumber(int[] nums) {
+    /*public int singleNumber(int[] nums) {
         Map<Integer,Integer> ResMap = new HashMap<>();
         for (int i :nums){
             if (!ResMap.containsKey(i)){
@@ -35,5 +38,16 @@ public class SingleNumber {
             }
         }
         return -1;
+    }*/
+
+    //solve-2:异或(^) (只有1个元素出现1次，其余都是出现2次，所以2次的元素异或完就只剩下1次的元素)
+    public int singleNumber(int[] nums) {
+        //异或(^)：同为假，异为真
+        int temp = 0;  //任何数异或0后，都等于本身（由异或定义得）
+        for(int i :nums){
+            temp^=i;
+        }
+        return temp;
+
     }
 }
