1d9daca70e6e159bcc970b16de7a7f17e9da9995
jam <97245251@qq.com>
2018-12-03 00:45:14 +0800

    144. 二叉树的前序遍历

--- src/main/java/top/yousite/leetcode/tree/middle/BinaryTreePreorderTraversal.java
+++ src/main/java/top/yousite/leetcode/tree/middle/BinaryTreePreorderTraversal.java
@@ -10,10 +10,32 @@ public class BinaryTreePreorderTraversal {
      */
     //递归
     List<Integer> list = new ArrayList<>();
+
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root == null) {return list;};
+        if (root == null) {
+            return list;
+        }
         list.add(root.val);
         preorderTraversal(root.left);
         return preorderTraversal(root.right);
+    }
+
+    //非递归实现
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null) return res;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            /*int size=stack.size();
+            for(int i =0;i<size;i++){*/     //bfs这步是为了得到各层次
+            TreeNode temp = stack.pop();
+            if(temp.right!=null) stack.push(temp.right);
+            if(temp.left!=null) stack.push(temp.left);
+            res.add(temp.val);
+            //}
+
+        }
+        return res;
     }
 }
