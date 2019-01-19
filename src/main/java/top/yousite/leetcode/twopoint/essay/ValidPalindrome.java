2adcf55c26810e16a74f577b09527e18bab05270
jam <97245251@qq.com>
2019-01-19 20:46:43 +0800

    125. 验证回文串

--- src/main/java/top/yousite/leetcode/twopoint/essay/ValidPalindrome.java
+++ src/main/java/top/yousite/leetcode/twopoint/essay/ValidPalindrome.java
@@ -2,40 +2,35 @@ package java.top.yousite.leetcode.twopoint.essay;
 
 public class ValidPalindrome {
     /**
-     *125. 验证回文串
-     *
+     * 125. 验证回文串
      */
     //思路，碰到不是字母，数字的，指针前进或后退
-    public boolean isPalindrome(String s) {
-        //双指针
-        char[] chars =s.toCharArray();
-        int low = 0;
-        int hig = chars.length-1;
-        while (low<=hig){
-            if (!checkNumOrLetter(chars[hig])){
-                hig--;
+    public boolean isPalindrome (String s){
+        //考虑字母与数字
+        if (s == null || s.length() == 0) return true;
+        char[] chars = s.toCharArray();
+        int begin =0;
+        int end = s.length()-1;
+        while (begin<=end){
+            if ( !vaild(chars[begin]) ) begin++;
+            else if ( !vaild(chars[end]) ) end--;
+            else if (Character.toUpperCase(chars[begin])  == Character.toUpperCase(chars[end])){
+                begin++;
+                end--;
             }
-            else if (!checkNumOrLetter(chars[low])){
-                low++;
-            }
-            else {
-                if (((chars[low]+32-'a')%32)!=((chars[hig]+32-'a')%32)){
-                    return false;
-                }else{
-                    low++;
-                    hig--;
-                }
-            }
+            else
+                return false;
+
         }
-        return  true;
+        return true;
+
     }
 
-    public boolean checkNumOrLetter(char ch){
-        if ('a'<=ch && ch<='z' ) return true;
-        if ('A'<=ch && ch<='Z') return true;
-        if(ch>='0'&&ch<='9')  return true;
+    private boolean vaild(char ch){
+        if (ch>='a'&&ch<='z') return true;
+        if (ch>='A'&&ch<='Z') return true;
+        if (ch>='0'&ch<='9') return true;
         return false;
-
     }
 
 }
