55c571dd7df27f8285112eb699cb23ed2555dfc7
jam <97245251@qq.com>
2019-01-19 19:55:27 +0800

    7. 整数反转

---
+++ src/main/java/top/yousite/leetcode/math/essay/Reverse.java
@@ -0,0 +1,17 @@
+package java.top.yousite.leetcode.math.essay;
+
+public class Reverse {
+    /**
+     * 7. 整数反转
+     */
+    //要考虑溢出
+    public int reverse(int x) {
+        long res = 0;
+        while(x!=0){
+            res = res*10 + x%10;
+            x/=10;
+            if(res>Integer.MAX_VALUE ||res<Integer.MIN_VALUE) return 0;
+        }
+        return (int)res;
+    }
+}
