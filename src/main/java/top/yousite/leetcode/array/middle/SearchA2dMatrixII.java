4d4cd539a3424891f3ad5a82d7967d64b1747fed
jam <97245251@qq.com>
2019-03-05 09:50:38 +0800

    240. 搜索二维矩阵 II

---
+++ src/main/java/top/yousite/leetcode/array/middle/SearchA2dMatrixII.java
@@ -0,0 +1,27 @@
+package java.top.yousite.leetcode.array.middle;
+
+public class SearchA2dMatrixII {
+    /**
+     * 240. 搜索二维矩阵 II
+     */
+
+    /**
+     * 思路:
+     *  右上角开始
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = col-1; i >=0; i--) {
+            if (matrix[0][i] == target) return true;
+            if (matrix[0][i]>target) continue;
+            if(matrix[0][i]<target){
+                for (int j = 0; j < row; j++) {
+                    if (matrix[j][i] == target)return true;
+                }
+            }
+        }
+        return false;
+    }
+}
