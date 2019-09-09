3604c51c78e78b07ad142d3aec664b563bedb384
jam <97245251@qq.com>
2019-09-09 21:30:27 +0800

    341. 扁平化嵌套列表迭代器

---
+++ src/main/java/top/yousite/leetcode/stack/middle/NestedIterator.java
@@ -0,0 +1,64 @@
+package top.yousite.leetcode.stack.middle;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+
+/**
+ * 341. 扁平化嵌套列表迭代器
+ */
+//思路:dfs+队列
+public class NestedIterator implements Iterator<Integer> {
+
+    Queue<Integer> queue= new LinkedList<>();
+    public NestedIterator(List<NestedInteger> nestedList) {
+        dfs(nestedList);
+    }
+
+    @Override
+    public Integer next() {
+        if (hasNext()){
+            return queue.poll();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return queue.size()>0;
+    }
+
+    private void dfs(List<NestedInteger> nestedList){
+        for(NestedInteger nest: nestedList){
+            if (nest.isInteger()){
+                queue.offer(nest.getInteger());
+            }else{
+                dfs(nest.getList());
+            }
+        }
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
